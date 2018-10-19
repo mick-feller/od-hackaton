34b8492ea73409656b2ed9a19cc1f81620473343
Mick-Feller <mick.feller@officedepot.com>
2018-10-18 20:39:18 -0400

    added login and google analytics

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,9 @@ import { renderRoutes } from 'react-router-config';
 import configureStore from 'store/index';
 import routes from 'routes/routes';
 import reducers from 'reducers/reducers';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-127724891-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 if (process.env.NODE_ENV === 'development') {
   const { registerObserver } = require('react-perf-devtool');
